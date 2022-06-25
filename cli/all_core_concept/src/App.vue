2719d8e92fcbf0c21f0612e5a94d27f0f8b65081
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-06-25 11:40:57 +0600

    component and props

--- cli/all_core_concept/src/App.vue
+++ cli/all_core_concept/src/App.vue
@@ -1,3 +1,26 @@
+<script >
+import Developer from "./components/Developer.vue";
+
+export default {
+  components: {
+    Developer
+  },
+
+  data() {
+    return {
+      name: "Md Mohimenol Islam",
+      type: "Front End Developer",
+      stackName: "React js, Vus js, Nuxt js",
+      jobExperience: 1,
+    }
+  }
+}
+</script>
+
+
 <template>
-<h2>Hello Vue CLI</h2>
+  <h2>Welcome {{ name }} to Vue js world!!</h2>
+  <div>
+    <Developer :name="name" :type="type" :stack-name="stackName" :job-experience="jobExperience" />
+  </div>
 </template>
---
+++ cli/all_core_concept/src/components/Developer.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+    // one way
+    // props: ["name", "type"],
+
+    // another way
+    props: {
+        name: String,
+        type: String,
+        "stack-name": String,
+        jobExperience: Number
+    },
+    data() {
+        return {
+            t: "",
+        };
+    },
+};
+</script>
+<template>
+    <div>
+        <h2>Developer Info</h2>
+        <h3>Name: {{ name }}</h3>
+        <h3>Type: {{ type }}</h3>
+        <h3>Stack: {{ stackName }}</h3>
+        <h3>Job Experience: {{ jobExperience }} year</h3>
+    </div>
+</template>
