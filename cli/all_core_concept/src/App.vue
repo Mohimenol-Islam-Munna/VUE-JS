1cd11ce9ff7ac7a10b4e373f340a52f851554e6d
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-06-25 12:00:58 +0600

    event with parameter

--- cli/all_core_concept/src/App.vue
+++ cli/all_core_concept/src/App.vue
@@ -13,14 +13,32 @@ export default {
       stackName: "React js, Vus js, Nuxt js",
       jobExperience: 1,
     }
-  }
+  },
+  methods: {
+    nameChangeHandler(nameArg) {
+      console.log("name change handler : ", nameArg);
+      this.name = nameArg;
+    }
+  },
 }
 </script>
 
 
 <template>
-  <h2>Welcome {{ name }} to Vue js world!!</h2>
-  <div>
-    <Developer :name="name" :type="type" :stack-name="stackName" :job-experience="jobExperience" />
+  <div style="width: 50%; margin: auto; border: 2px solid salmon; padding: 20px; border-radius: 15px;">
+    <h2>Welcome {{ name }} to Vue js world!!</h2>
+
+    <button v-on:click="nameChangeHandler('Md Ifti')" style="margin-right: 10px;">
+      Click To Chanage Name
+    </button>
+
+    <button @click="nameChangeHandler('New Md Ifti')">
+      Again Click To Chanage Name
+    </button>
+
+
+    <div>
+      <Developer :name="name" :type="type" :stack-name="stackName" :job-experience="jobExperience" />
+    </div>
   </div>
 </template>
